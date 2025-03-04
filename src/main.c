29e5a5e876980fdba1f36057b530140746749d30
harikrishnamohann <harikrishnamohan@proton.me>
2025-03-04 20:04:20 +0530

    status line, game over detection and in-place number highlighting are implemented

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <ncurses.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <stdint.h>
 #include "../include/arena.h"
@@ -17,22 +18,31 @@ struct gmatrix {
 
 static void swap(int *x, int *y) { *x = *x ^ *y; *y = *x ^ *y; *x = *x ^ *y; }
 
-void print_matrix(const struct gmatrix gmatrix) {
-  int height, width;
+// return 1 if elements are sorted.
+int render_matrix(const struct gmatrix gmat) {
+  int height, width, count = 0, in_place = 0, n_elements = gmat.order * gmat.order;
   getmaxyx(stdscr, height, width);
   erase();
-  for (int i = 0; i < gmatrix.order; i++) {
-    move(((height - gmatrix.order) / 2) + i, (width - gmatrix.order * 4) / 2);
-    for (int j = 0; j < gmatrix.order; j++) {
-      if (i == gmatrix.curs_x && j == gmatrix.curs_y) {
+  for (int i = 0; i < gmat.order; i++) {
+    move(((height - gmat.order) / 2) + i, (width - gmat.order * 4) / 2);
+    for (int j = 0; j < gmat.order; j++) {
+      if (i == gmat.curs_x && j == gmat.curs_y) {
         printw("    ");
       } else {
-        printw("%3d ", gmatrix.mat[i][j]);
+        if (count == gmat.mat[i][j] - 1) {
+          in_place++;
+          attron(A_BOLD);
+          printw("%3d ", gmat.mat[i][j]);
+          attroff(A_BOLD);
+        } else {
+          printw("%3d ", gmat.mat[i][j]);
+        }
+        count++;
       }
     }
     printw("\n");
   }
-  refresh();
+  return (in_place == n_elements - 1);
 }
 
 direction decode_dir(int ch) {
@@ -45,18 +55,23 @@ direction decode_dir(int ch) {
   return invalid;
 }
 
-void mov_zero(struct gmatrix* gmatrix, int ch) {
+int mov_zero(struct gmatrix* gmatrix, int ch) {
   int x = gmatrix->curs_x, y = gmatrix->curs_y;
   switch (decode_dir(ch)) {
-    case up : if (x != 0) x--; break;
-    case down : if (x != gmatrix->order - 1) x++; break;
-    case right : if (y != gmatrix->order - 1) y++; break;
-    case left : if (y != 0) y--; break;
-    case invalid : break;
+    case up :
+      if (x == 0) return 0; else x--; break;
+    case down :
+      if (x == gmatrix->order - 1) return 0; else x++; break;
+    case right :
+      if (y == gmatrix->order - 1) return 0; else y++; break;
+    case left :
+      if (y == 0) return 0; else y--; break;
+    case invalid : return 0;
   }
   swap(&(gmatrix->mat[x][y]), &(gmatrix->mat[gmatrix->curs_x][gmatrix->curs_y]));
   gmatrix->curs_x = x;
   gmatrix->curs_y = y;
+  return 1;
 }
 
 void shuffle_matrix(struct gmatrix* gmatrix) {
@@ -96,23 +111,59 @@ struct gmatrix gmatrix_init(Arena* arena, int order) {
 
 void debug_print_matrix(struct gmatrix mat);
 
+void print_status_line(size_t count, direction key, const char* msg) {
+  int x, y, current_x, current_y;
+  getyx(stdscr, current_x, current_y);
+  getmaxyx(stdscr, y, x);
+  int msg_start = (x - strlen(msg)) / 2;
+
+  mvprintw(y - 1, 1, " moves: %2zu", count);
+  mvprintw(y - 1, msg_start, "%s", msg);
+
+  move(y - 1, x - 2);
+  switch(decode_dir(key)) {
+    case up: printw("U"); break;
+    case down: printw("D"); break;
+    case left: printw("L"); break;
+    case right: printw("R"); break;
+    case invalid: break;
+  }
+  mvchgat(y - 1, 0, -1, A_REVERSE, 1, NULL);
+  
+  move(current_x, current_y);
+}
+
 int main(int argc, char* argv[]) {
+  int difficulty = 4;
   srand(time(NULL));
   Arena *arena = arena_init(ARENA_128);
-  struct gmatrix gmat = gmatrix_init(arena, 4);
+  struct gmatrix gmat = gmatrix_init(arena, difficulty);
 
   initscr();
   noecho();
   curs_set(0);
   keypad(stdscr, TRUE);
+  cbreak();
 
-  int ch;
-  print_matrix(gmat);
-  while ((ch = getch()) != 'q') {
-    mov_zero(&gmat, ch);
-    print_matrix(gmat);
+  int ch = 0, moves = 0, completed = 0;
+  char *msg = "sort the matrix";
+  render_matrix(gmat);
+  print_status_line(moves, ch, msg);
+  while (ch != 'q') {
+    ch = getch();
+    moves += mov_zero(&gmat, ch);
+    completed = render_matrix(gmat);
+    if (completed) {
+    print_status_line(moves, ch, msg);
+      msg = "You Won! press 'q' to quit!";
+      ch = 'q';
+    }
+    print_status_line(moves, ch, msg);
     refresh();
-  }
+  } 
+
+  if (!completed) print_status_line(moves, ch, "interrupted. press 'q' to quit");
+  while(getch() != 'q');
 
   endwin();
   arena_free(arena);
