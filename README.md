24509b5e9fd2fcff1891ba17ee9254fdc5f3ac08
harikrishnamohann <harikrishnamohan@proton.me>
2025-03-05 01:18:46 +0530

    menu system added

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 ### Todo
 - sound effects
 - ~~highlight characters that are in place~~
-- a menu for choosing difficulty
+- ~~a menu for choosing difficulty~~
 - ~~detect when game is completed~~
 - ~~status line for showing some informations such as counter, difficulty etc~~
 - save highscore
 - display leaderboard on game completion
+- undo redo mechanism
--- src/main.c
+++ src/main.c
@@ -2,12 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <stdint.h>
 #include "../include/arena.h"
 
 typedef enum {
-  invalid, up, down, left, right
-} direction;
+  key_invalid,
+  key_up,
+  key_down,
+  key_left,
+  key_right,
+  key_return,
+} Direction;
+
+typedef enum {
+  mode_invalid,
+  mode_easy = 0,
+  mode_normal = 1,
+  mode_hard = 2,
+  mode_custom = 3,
+  mode_exit = 4,
+} Mode;
 
 struct gmatrix {
   uint16_t curs_x;
@@ -19,7 +32,7 @@ struct gmatrix {
 static void swap(int *x, int *y) { *x = *x ^ *y; *y = *x ^ *y; *x = *x ^ *y; }
 
 // return 1 if elements are sorted.
-int render_matrix(const struct gmatrix gmat) {
+int update_matrix_view(const struct gmatrix gmat) {
   int height, width, count = 0, in_place = 0, n_elements = gmat.order * gmat.order;
   getmaxyx(stdscr, height, width);
   erase();
@@ -45,28 +58,29 @@ int render_matrix(const struct gmatrix gmat) {
   return (in_place == n_elements - 1);
 }
 
-direction decode_dir(int ch) {
+Direction decode_dirn(int ch) {
   switch (ch) {
-    case 's' : case 'j' : case KEY_DOWN : return down;
-    case 'w' : case 'k' : case KEY_UP : return up;
-    case 'd' : case 'l' : case KEY_RIGHT : return right;
-    case 'a' : case 'h' : case KEY_LEFT : return left;
+    case 's' : case 'j' : case KEY_DOWN : return key_down;
+    case 'w' : case 'k' : case KEY_UP : return key_up;
+    case 'd' : case 'l' : case KEY_RIGHT : return key_right;
+    case 'a' : case 'h' : case KEY_LEFT : return key_left;
+    case KEY_ENTER : case '\n' :  return key_return;
   }
-  return invalid;
+  return key_invalid;
 }
 
 int mov_zero(struct gmatrix* gmatrix, int ch) {
   int x = gmatrix->curs_x, y = gmatrix->curs_y;
-  switch (decode_dir(ch)) {
-    case up :
+  switch (decode_dirn(ch)) {
+    case key_up :
       if (x == 0) return 0; else x--; break;
-    case down :
+    case key_down :
       if (x == gmatrix->order - 1) return 0; else x++; break;
-    case right :
+    case key_right :
       if (y == gmatrix->order - 1) return 0; else y++; break;
-    case left :
+    case key_left :
       if (y == 0) return 0; else y--; break;
-    case invalid : return 0;
+    default : return 0;
   }
   swap(&(gmatrix->mat[x][y]), &(gmatrix->mat[gmatrix->curs_x][gmatrix->curs_y]));
   gmatrix->curs_x = x;
@@ -109,7 +123,7 @@ struct gmatrix gmatrix_init(Arena* arena, int order) {
   return gmat;
 }
 
-void print_status_line(size_t count, direction key, const char* msg) {
+void print_status_line(size_t count, Direction key, const char* msg) {
   int x, y, current_x, current_y;
   getyx(stdscr, current_x, current_y);
   getmaxyx(stdscr, y, x);
@@ -119,25 +133,105 @@ void print_status_line(size_t count, direction key, const char* msg) {
   mvprintw(y - 1, msg_start, "%s", msg);
 
   move(y - 1, x - 2);
-  switch(decode_dir(key)) {
-    case up: printw("U"); break;
-    case down: printw("D"); break;
-    case left: printw("L"); break;
-    case right: printw("R"); break;
-    case invalid: break;
+  switch(decode_dirn(key)) {
+    case key_up: printw("U"); break;
+    case key_down: printw("D"); break;
+    case key_left: printw("L"); break;
+    case key_right: printw("R"); break;
+    default: break;
   }
   mvchgat(y - 1, 0, -1, A_REVERSE, 1, NULL);
   
   move(current_x, current_y);
 }
 
-void debug_print_matrix(struct gmatrix mat);
+void show_menu(Direction key, uint *highlight) {
+  int height, width, x, y;
+  char* menu_items[] = {
+    "  easy  ",
+    " normal ",
+    "  hard  ",
+    " custom ",
+    "  exit  ",
+  };
+  size_t menu_size = sizeof(menu_items) / sizeof(char*);
+
+  if (key == key_down)
+    *highlight = (*highlight + 1) % menu_size;
+  else if (key == key_up)
+    *highlight = (*highlight == 0) ? menu_size - 1 : *highlight - 1;
+
+  getmaxyx(stdscr, height, width);
+  x = (width - strlen("Welcome to uni-void!")) / 2;
+  y = (height - (3 + menu_size)) / 2;
+
+  erase();
+  attron(A_BOLD );
+  mvprintw(y++, x, "WELCOME TO UNI-VOID!");
+  attroff(A_BOLD);
+  y++;
+  attron(A_UNDERLINE);
+  mvprintw(y++, x, "Choose a difficulty");
+  attroff(A_UNDERLINE);
+
+  for (size_t i = 0; i < menu_size; i++) {
+    x = (width - strlen(menu_items[i])) / 2;
+    if (i == *highlight) {
+      attron(A_REVERSE | A_BOLD);
+      mvprintw(y++, x, "%s", menu_items[i]);    
+      attroff(A_REVERSE | A_BOLD);
+    } else {
+      mvprintw(y++, x, "%s", menu_items[i]);    
+    }
+  } 
+}
+
+int input_difficulty() {
+  erase();
+  char difficulty[10];
+  int x, y, height, width;
+  getmaxyx(stdscr, height, width);
+  char* query = "Enter order of square matrix: ";
+  x = (width - strlen(query)) / 2;
+  y = height / 2;
+  mvprintw(y, x, "%s", query);
+  echo();
+  nocbreak();
+  curs_set(1);
+  refresh();
+  getstr(difficulty);
+  noecho();
+  cbreak();
+  curs_set(0);
+  return atoi(difficulty);
+}
+
+int get_difficulty() {
+  uint highlight = 0;
+  int ch;
+  Direction key;
+  do {
+    key = decode_dirn(ch);
+    if (key == key_return) {
+      if (highlight == mode_exit) {
+        break;
+      } else if (highlight == mode_custom) {
+        return input_difficulty();
+      } else {
+        return highlight + 3;
+      }
+    } else {
+      show_menu(key, &highlight);
+    }
+    refresh();
+  } while ((ch = getch()) != 'q');
+  endwin();
+  exit(0);
+}
 
 int main(int argc, char* argv[]) {
-  int difficulty = 3;
+  int difficulty = 0;
   srand(time(NULL));
-  Arena *arena = arena_init(ARENA_128);
-  struct gmatrix gmat = gmatrix_init(arena, difficulty);
 
   initscr();
   noecho();
@@ -145,14 +239,20 @@ int main(int argc, char* argv[]) {
   keypad(stdscr, TRUE);
   cbreak();
 
+
+  difficulty = get_difficulty();
+
+  Arena *arena = arena_init(ARENA_128);
+  struct gmatrix gmat = gmatrix_init(arena, difficulty);
+
   int ch = 0, moves = 0, completed = 0;
   char *msg = "sort the matrix";
-  render_matrix(gmat);
+  update_matrix_view(gmat);
   print_status_line(moves, ch, msg);
   while (ch != 'q') {
     ch = getch();
     moves += mov_zero(&gmat, ch);
-    completed = render_matrix(gmat);
+    completed = update_matrix_view(gmat);
     if (completed) {
       msg = "You Won! press 'q' to quit!";
       ch = 'q';
@@ -168,27 +268,3 @@ int main(int argc, char* argv[]) {
   arena_free(arena);
   return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-void debug_print_matrix(struct gmatrix mat) {
-  for (int i = 0; i < mat.order; i++) {
-    for (int j = 0; j < mat.order; j++) {
-      printf("%3d ", mat.mat[i][j]);
-    }
-    printf("\n");
-  }
-  printf("\norder: %d\n", mat.order);
-  printf("curs(%d, %d)\n", mat.curs_x, mat.curs_y);
-}
-
